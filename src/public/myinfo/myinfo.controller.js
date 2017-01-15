ff3461dc22702dc64ba807eeb4f1d66652d567bb
Mutasim <gsm@yahoo.com>
2017-01-15 16:01:58 +0530

    assignment5

---
+++ src/public/myinfo/myinfo.controller.js
@@ -0,0 +1,12 @@
+(function () {
+  "use strict";
+
+angular.module('public')
+.controller('myinfoController', myinfoController);
+
+function myinfoController(RegistrationController) {
+  var $ctrl = this;
+  // $srtl. = RegistrationController
+
+}
+})();
---
+++ src/public/signup/signup.controller.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+angular.module('public')
+.controller('RegistrationController', RegistrationController);
+
+
+function RegistrationController() {
+  var reg = this;
+
+  reg.submit = function () {
+    reg.completed = true;
+  };
+}
+
+})();
